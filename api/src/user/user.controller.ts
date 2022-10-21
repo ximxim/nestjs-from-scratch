dd8c2c4b686a171f9d93ff0a6b4b3f8e95454955
Azim Ahmed <azim.ahmed7@gmail.com>
2022-10-21 11:13:43 -0400

    nestjsx crud enhancement

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -4,10 +4,29 @@ import { JWTAuthGuard } from '../auth/jwt-auth-guard';
 import { User } from '../typeorm';
 import { Crud, CrudController } from '@nestjsx/crud';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Crud({
   model: {
     type: User,
+  },
+  query: {
+    exclude: ['password'],
+    alwaysPaginate: true,
+    limit: 2,
+    join: {
+      profile: {
+        eager: true,
+      },
+    },
+  },
+  dto: {
+    create: CreateUserDto,
+    update: UpdateUserDto,
+  },
+  routes: {
+    exclude: ['replaceOneBase', 'createManyBase'],
   },
 })
 @ApiTags('user')
