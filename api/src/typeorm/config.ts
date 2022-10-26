cbb406bb9a8fde2f226ce4abe8b9016a66d9b6f5
Azim Ahmed <azim.ahmed7@gmail.com>
2022-10-26 12:47:56 -0400

    typeorm logger

--- api/src/typeorm/config.ts
+++ api/src/typeorm/config.ts
@@ -1,8 +1,63 @@
-import { DataSource } from 'typeorm';
+import { Logger as NestLogger } from '@nestjs/common';
+import { DataSource, Logger as TypeOrmLogger } from 'typeorm';
 import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
 
 import entities from './entities';
 import migrations from './migrations';
+
+class DatabaseLogger implements TypeOrmLogger {
+  private readonly logger = new NestLogger('SQL');
+
+  logQuery(query: string, parameters?: unknown[]) {
+    this.logger.verbose(
+      `${query} -- Parameters: ${this.stringifyParameters(parameters)}`,
+    );
+  }
+
+  logQueryError(error: string, query: string, parameters?: unknown[]) {
+    this.logger.error(
+      `${query} -- Parameters: ${this.stringifyParameters(
+        parameters,
+      )} -- ${error}`,
+    );
+  }
+
+  logQuerySlow(time: number, query: string, parameters?: unknown[]) {
+    this.logger.warn(
+      `Time: ${time} -- Parameters: ${this.stringifyParameters(
+        parameters,
+      )} -- ${query}`,
+    );
+  }
+
+  logMigration(message: string) {
+    this.logger.verbose(message);
+  }
+
+  logSchemaBuild(message: string) {
+    this.logger.verbose(message);
+  }
+
+  log(level: 'log' | 'info' | 'warn', message: string) {
+    if (level === 'log') {
+      return this.logger.verbose(message);
+    }
+    if (level === 'info') {
+      return this.logger.verbose(message);
+    }
+    if (level === 'warn') {
+      return this.logger.warn(message);
+    }
+  }
+
+  private stringifyParameters(parameters?: unknown[]) {
+    try {
+      return JSON.stringify(parameters);
+    } catch {
+      return '';
+    }
+  }
+}
 
 export const TypeOrmOptions: SqliteConnectionOptions = {
   type: 'sqlite',
@@ -10,6 +65,8 @@ export const TypeOrmOptions: SqliteConnectionOptions = {
   entities,
   migrations,
   migrationsRun: true,
+  logging: true,
+  logger: new DatabaseLogger(),
 };
 
 export default new DataSource(TypeOrmOptions);
